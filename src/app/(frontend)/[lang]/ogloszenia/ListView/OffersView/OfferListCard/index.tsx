988d7604dd1853aba2fff416927499bf56cbc13d
Mateusz Muślewski <kontakt@muslewski.com>
2026-02-06 18:04:14 +0100

    get rid of offer list card

--- src/app/(frontend)/[lang]/ogloszenia/ListView/OffersView/OfferListCard/index.tsx
+++ src/app/(frontend)/[lang]/ogloszenia/ListView/OffersView/OfferListCard/index.tsx
@@ -47,7 +47,7 @@ export const OfferListCard = ({
     <Card className="w-full flex xl:flex-row flex-col py-0 sm:py-0 h-120 sm:h-130 xl:h-48  items-center bg-transparent bg-linear-to-r from-stone-200 dark:from-stone-900/60 to-background/35 rounded-2xl overflow-hidden">
       <Link
         href={`/ogloszenia/${slug}`}
-        className="w-full xl:max-w-3xs h-full bottom-0 top-0 rounded-2xl bg-white/10 relative overflow-hidden dark:shadow-[6px_0px_32px_2px_rgba(24,10,10,1)]"
+        className="w-full xl:max-w-3xs h-full bottom-0 top-0 rounded-2xl bg-white/10 relative overflow-hidden "
       >
         {imageUrl && (
           <Image
