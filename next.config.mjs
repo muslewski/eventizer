0ce35be3c7ceb9fb605bf5f70f7fb3bcb84e991a
Mateusz Muślewski <kontakt@muslewski.com>
2026-02-27 01:18:41 +0100

    Handle uploads of up to 60mb videos

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 const nextConfig = {
   experimental: {
     serverActions: {
-      bodySizeLimit: '5mb',
+      bodySizeLimit: '60mb',
     },
   },
 
