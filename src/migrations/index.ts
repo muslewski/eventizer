d18165f4be66a979513c438db7f409c334ac4135
Mateusz Muślewski <kontakt@muslewski.com>
2026-04-30 02:29:00 +0200

    feat(billing): add migration for subscription_plans.max_offers and users.downgraded_drafted_at

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20251217_102626_backup_before_switch from './20251217_102626_backup_before_switch';
 import * as migration_20260420_204841_add_offer_website from './20260420_204841_add_offer_website';
 import * as migration_20260423_120000_add_offer_upload_zoom from './20260423_120000_add_offer_upload_zoom';
+import * as migration_20260430_022634_add_billing_tier_fields from './20260430_022634_add_billing_tier_fields';
 
 export const migrations = [
   {
@@ -17,5 +18,10 @@ export const migrations = [
     up: migration_20260423_120000_add_offer_upload_zoom.up,
     down: migration_20260423_120000_add_offer_upload_zoom.down,
     name: '20260423_120000_add_offer_upload_zoom'
+  },
+  {
+    up: migration_20260430_022634_add_billing_tier_fields.up,
+    down: migration_20260430_022634_add_billing_tier_fields.down,
+    name: '20260430_022634_add_billing_tier_fields'
   },
 ];
